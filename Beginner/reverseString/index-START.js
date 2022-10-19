331628a1e8f61a8a3d73a8f5109a7f75bbe4f1bd
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-19 14:10:25 -0700

    completed reverseString tests

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -4,11 +4,43 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
 
-
+// Method Chaining
 function reverseString(text) {
-    // Code goes here
+    return text.split('')
+        .reverse()
+        .join('')
 }
 
+// Spread Operator + Method Chaining
+function reverseString(text) {
+    return [...text].reverse().join('')
+}
+
+// For Loop - O(n)
+function reverseString(text) {
+    let reversedText = ''
+    // for (let i = text.length - 1; i >= 0; i--) {
+    //     reversedText += text[i]
+    // }
+    for (let i = 0; i < text.length; i++) {
+        reversedText += text[text.length - 1 - i]
+    }
+    return reversedText
+}
+
+// Recursion
+function reverseString(text) {
+    if (text === '')
+        return ''
+    else
+        return reverseString(text.slice(1)) + text[0]
+}
+
+// Reduce Method
+function reverseString(text) {
+    return text.split('')
+        .reduce((reversedText, x, i) => text[i] + reversedText, '')
+}
 
 
 module.exports = reverseString
