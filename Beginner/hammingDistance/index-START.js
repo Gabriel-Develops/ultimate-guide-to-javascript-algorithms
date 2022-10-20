4c0cfdeff4f9b1ad5c0e1c704e40fefa3fec32bf
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-20 12:39:21 -0700

    completed hammingDistance

--- Beginner/hammingDistance/index-START.js
+++ Beginner/hammingDistance/index-START.js
@@ -7,7 +7,17 @@ E.g hammingDistance('rover', 'river') // should return 1
 
 
 function hammingDistance(stringA, stringB) {
-    // Code goes here
+    if (stringA.length !== stringB.length) return new Error('Strings must be of same length')
+
+    let totalDiff = 0
+    for (let i = 0; i < stringA.length; i++)  {
+        if (stringA[i] === stringB[i]) {
+        continue
+        } else {
+        totalDiff += 1
+        }
+    }
+    return totalDiff
 }
 
 
