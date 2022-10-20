3346ac53ab9798380b05e6a8edaa3dd5e1171fcb
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-20 12:56:35 -0700

    completed longestWord challenge

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -6,7 +6,14 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function longestWord(text) {
-    // Code goes here
+    let longestWord = {length: 0, text: ''}
+    for (let i = 0, textArr = text.split(' '); i < textArr.length; i++) {
+        if (textArr[i].length > longestWord.length) {
+            longestWord.length = textArr[i].length
+            longestWord.text = textArr[i]
+        }
+    }
+    return longestWord.text
 }
 
 
