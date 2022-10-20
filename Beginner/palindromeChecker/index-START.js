4fce926a64fa59bf7372e92c18a92a6baeccf274
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-20 11:41:37 -0700

    completed palindromeChecker

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -5,10 +5,31 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
-
+// For Loop - O(n)
 function palindromeChecker(text) {
-    v// Code goes here
+    for (let i = 0, j = text.length - 1; i < text.length; i++, j--) {
+        if (i === j || i > j)
+            return true
+        else if (text[i] === text[j])
+            continue
+        else
+            return false 
+    }
 }
+
+// Optimized For Loop
+function palindromeChecker(text) {
+    for (let i = 0; i < Math.floor(text.length / 2); i++) {
+        if (text[i].toLowerCase() === text[text.length - 1 - i].toLowerCase()) {
+            continue
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+
 
 
 
