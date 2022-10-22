ca427d48bf65e8b8bf8ee2dda4014770c9594a4e
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-21 17:57:28 -0700

    completed falsyBouncer challenge

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,9 +5,18 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
-
+// Filter method
 function falsyBouncer(array) {
-    // Code goes here
+    return array.filter(x => x)
+}
+
+// For Loop
+function falsyBouncer(array) {
+    const newArray = []
+    for (const element of array) {
+        element && newArray.push(element)
+    }
+    return newArray
 }
 
 
