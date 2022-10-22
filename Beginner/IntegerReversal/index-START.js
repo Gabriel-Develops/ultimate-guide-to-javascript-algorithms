c0c1c089a77af33017fa9829bdba0e2e44ad7393
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-21 23:25:43 -0700

    completed integerReversal challenge

--- Beginner/IntegerReversal/index-START.js
+++ Beginner/IntegerReversal/index-START.js
@@ -5,13 +5,39 @@ ordering of that which was received. E.g
 */
 
 
-function reverseString(text) {
-    return [...text].reduce((acc, char) => char + acc, '')
+// function reverseString(text) {
+//     return [...text].reduce((acc, char) => char + acc, '')
+// }
+
+// function reverseInteger(num) {
+//     let reversedNumber = parseInt(reverseString(num.toString()))
+//     return (reversedNumber * Math.sign(num))
+// }
+
+// For ... of loop
+function reverseInteger(num) {
+    let reverseStr = '', isNegative = ''
+    for (const c of num.toString()) {
+        if (c === '-') {
+            isNegative = '-'
+        } else {
+            reverseStr = c + reverseStr
+        }
+    }
+    return +`${isNegative}${+reverseStr}`
 }
 
+// Recursion
 function reverseInteger(num) {
-    let reversedNumber = parseInt(reverseString(num.toString()))
-    return (reversedNumber * Math.sign(num))
+    // Termination Conditional
+    if (num.toString().length <= 1) {
+        return +num
+    // Case of negative sign
+    } else if (num.toString()[0] === '-') {
+        return +('-' + reverseInteger(num.toString().slice(1)))
+    } else {
+        return +(reverseInteger(num.toString().slice(1)) + num.toString()[0])
+    }
 }
 
 
