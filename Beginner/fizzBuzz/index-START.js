aec8cebaf3a38ef54e8ca016b41c50e8b8647aab
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-23 18:14:43 -0700

    completed fizzbuzz challenge

--- Beginner/fizzBuzz/index-START.js
+++ Beginner/fizzBuzz/index-START.js
@@ -6,9 +6,29 @@
 */
 
 
-
+// For Loop
 function fizzBuzz(n) {
-    // Code goes here
+    for (let i = 1; i <= n; i ++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if (i % 3 === 0) {
+            console.log('fizz')
+        } else if (i % 5 === 0) {
+            console.log('buzz')
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+// For Loop with Concatenation
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        let result = ''
+        if (i % 3 === 0) result += 'fizz'
+        if (i % 5 === 0) result += 'buzz'
+        console.log(result || i)
+    }
 }
 
 
