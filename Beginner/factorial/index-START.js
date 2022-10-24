6ae54170663b1f9db861d6afb9dae1c4c65e9ec2
Gabriel Rodriguez <garodri97@gmail.com>
2022-10-23 18:14:00 -0700

    completed factorial challenge

--- Beginner/factorial/index-START.js
+++ Beginner/factorial/index-START.js
@@ -4,16 +4,40 @@ the provided integer(n). E.g
     factorial(5) // should return 120
 */
 
-
+// Recursion Method
 function factorial(n, memo) {
-    memo = memo || {}
-    if(memo[n]){
-        return memo[n]
-    }  
+    if (n === 1 || n === 0) {
+        return 1
+    } else if (n < 1) {
+        return -1 * factorial(n * -1)
+    } else {
+        return n * factorial(--n)
+    }
+}
+
+// For Loop
+function factorial(n, memo) {
     if (n === 0) {
         return 1
     }
-    return memo[n] = n * factorial(n - 1, memo)
+    let product = 1
+    for (let i = 1, end = Math.abs(n); i <= end; i++) {
+        product *= i
+    }
+    return Math.sign(n) * product
+}
+
+// While Loop
+function factorial(n, memo) {
+    if (n === 0) 
+        return 1
+    
+    let product = 1, number = Math.abs(n)
+    while (number > 1) {
+        product *= number
+        number--
+    }
+    return Math.sign(n) * product
 }
 
 module.exports = factorial
